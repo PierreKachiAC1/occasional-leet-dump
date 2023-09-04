a2be4b9029754f038aff18a2fccd044dc4865bed
Pierre K <kachi.pierre@gmail.com>
2023-09-04 11:44:44 +0300

    "better triangle"

--- triangle/Main.java
+++ triangle/Main.java
@@ -3,9 +3,7 @@ package triangle;
 
 public class Main {
     public static void main(String[] args) {
-//        triangle(5);
-//        System.out.println(myPow2(2.1,3));
-//        System.out.println(recrev1("Hola"));
+
        triangle(5);
 
     }
@@ -14,29 +12,23 @@ public class Main {
 
     public static void triangle(int a){
 
-        for (int i = 1; i <= a; i++) {
+        for (int i = 0; i <= a; i++) {
             for (int j=i;j<a;j++){
                 System.out.print(" ");
             }
-            for (int z=1;z<=i;z++){
-//                if(i==a || z==i || z==1)
-                    System.out.print("* ");
-//                else
-//                    System.out.print(" ");
+            for(int d =a;d>i;d--){
+                System.out.print(" ");
+            }
+            for (int z=1;z<=i*2-1;z++){
+
+
+                System.out.print("* ");
+
 
             }
             System.out.println();
         }
 
-        for (int i = 0; i <= a; i++) {
-            for (int j=0;j<a;j++){
-                System.out.print(i+""+j+" ");
-            }
-//            for (int z=0;z<i;z++){
-//                System.out.print("* ");
-//
-//            }
-            System.out.println();
-        }
+
     }
 }
